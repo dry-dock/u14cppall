695ec501b700d03acd4960e30acad76c9961cc1c
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-15 13:35:11 +0530

    update the gcc version 7.3 to 8

--- install.sh
+++ install.sh
@@ -8,15 +8,15 @@ apt-get clean
 apt-get update
 echo "======================== Cleaned apt-get ======================"
 
-echo "======================= Installing gcc 7.2 ======================"
+echo "======================= Installing gcc 8 ======================"
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get update
 apt-get install -y \
-  gcc-7=7.3* \
-  g++-7=7.3*
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 50 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+  gcc-8=8* \
+  g++-8=8*
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 50 --slave /usr/bin/g++ g++ /usr/bin/g++-8
 gcc --version
-echo "================== Successfully Installed gcc 7.2 ==============="
+echo "================== Successfully Installed gcc 8 ==============="
 
 apt-get install \
   autoconf=2.69* \
@@ -24,7 +24,7 @@ apt-get install \
   ccache=3.1* \
   libssl-dev:amd64=1.0*
 
-CLANG_VERSION=5.0.1
+CLANG_VERSION=6.0.0
 echo "==================== Installing clang $CLANG_VERSION ==================="
 wget -nv http://releases.llvm.org/"$CLANG_VERSION"/clang+llvm-"$CLANG_VERSION"-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 tar xf clang+llvm-"$CLANG_VERSION"-x86_64-linux-gnu-ubuntu-14.04.tar.xz
